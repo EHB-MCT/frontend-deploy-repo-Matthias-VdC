51f1cd3abd2fbf1b93278a85eb69fd69e20a29ab
MatthiasVandeCasteele <matthias.van.de.casteele@student.ehb.be>
2022-05-09 15:47:59 +0200

    check if user is in voice channel for !play

--- commands/play.js
+++ commands/play.js
@@ -5,40 +5,43 @@ import ytdl from "ytdl-core";
 const queue = [];
 
 export default async function play(message) {
-    console.log("Joining voice channel!");
-    const connection = joinVoiceChannel({
-        channelId: message.member.voice.channelId,
-        guildId: message.member.guild.id,
-        selfDeaf: true,
-        selfMute: false,
-        adapterCreator: message.guild.voiceAdapterCreator,
-    });
+    if (message.member.voice.channel !== null) {
+        console.log("Joining voice channel!");
+        const connection = joinVoiceChannel({
+            channelId: message.member.voice.channelId,
+            guildId: message.member.guild.id,
+            selfDeaf: true,
+            selfMute: false,
+            adapterCreator: message.guild.voiceAdapterCreator,
+        });
+        try {
+            const songUrl = message.content.split(" ");
+            console.log(songUrl);
+            const songInfo = await ytdl.getInfo(songUrl[1]);
 
-    try {
-        const songUrl = message.content.split(" ");
-        console.log(songUrl);
-        const songInfo = await ytdl.getInfo(songUrl[1]);
+            const song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url,
+                author: songInfo.videoDetails.author.name,
+            }
 
-        const song = {
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url,
-            author: songInfo.videoDetails.author.name,
+            message.reply(`Now playing: ${song.title} from ${song.author}`);
+
+            console.log(song);
+
+            const stream = ytdl(songUrl[1], { filter: "audioonly" });
+            const player = createAudioPlayer();
+            const resource = createAudioResource(stream, { inlineVolume: true });
+            resource.volume.setVolume(0.06);
+            (async function play() {
+                await player.play(resource);
+                connection.subscribe(player);
+            })();
+        } catch (err) {
+            connection.destroy();
+            throw err;
         }
-
-        message.reply(`Now playing: ${song.title} from ${song.author}`);
-
-        console.log(song);
-
-        const stream = ytdl(songUrl[1], { filter: "audioonly" });
-        const player = createAudioPlayer();
-        const resource = createAudioResource(stream, { inlineVolume: true });
-        resource.volume.setVolume(0.06);
-        (async function play() {
-            await player.play(resource);
-            connection.subscribe(player);
-        })();
-    } catch (err) {
-        connection.destroy();
-        throw err;
+    } else {
+        message.reply("You are currently not in a voice channel!");
     }
 }
